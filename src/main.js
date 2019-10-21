c20d8e97dc14c0306a7e0b0ab64305a0771eb28e
Richard <richardsaseun@gmail.com>
2019-10-21 03:11:56 +0100

    chore(prettier-config):add prettier configuration
    Signed-off-by: Richard <richardsaseun@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store/store";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store/store"
 
-import "./sass/app.scss";
-Vue.config.productionTip = false;
+import "./sass/app.scss"
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
