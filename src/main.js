43cb66689e658c311eddcc29ae11832f2391395f
Richard Saseun <richardsaseun@gmail.com>
2019-12-30 21:24:58 +0100

    chore(bootstrap):  add bostrap-vue to the main.js but commented

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 
+// install bootstrap-vue
+// npm install vue bootstrap-vue bootstrap
+
+// Bootstrap Config
+// import BootstrapVue from "bootstrap-vue";
+// Vue.use(BootstrapVue);
+
 import "./sass/app.scss";
 Vue.config.productionTip = false;
 
